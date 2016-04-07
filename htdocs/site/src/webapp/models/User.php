da566b7237ce3dce3eae3866ac5f8f9c1d8e29a5
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-04-07 11:25:51 +0200

    Updated delete query

--- htdocs/site/src/webapp/models/User.php
+++ htdocs/site/src/webapp/models/User.php
@@ -8,7 +8,7 @@ class User
 {
     const INSERT_QUERY          = "INSERT INTO users(username, password, email, bio, isadmin) VALUES(?, ?, ?, ?, ?)";
     const UPDATE_QUERY          = "UPDATE users SET username=?, password=?, email=?, bio=?, isadmin=? WHERE id=?";
-    const DELETE_QUERY          = "DELETE FROM users WHERE id='%s'";
+    const DELETE_QUERY          = "DELETE FROM users WHERE id=?";
     const FIND_BY_NAME_QUERY    = "SELECT * FROM users WHERE username=?";
     const FIND_BY_ID_QUERY      = "SELECT * FROM users WHERE id=?";
     const ALL_QUERY             = "SELECT * FROM users";
